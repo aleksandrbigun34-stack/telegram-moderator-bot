8f247b440721d4189c83c228256fec79ef15e8a0
aleksandrbigun34-stack <aleksandrbigun34@gmail.com>
2026-07-15 16:19:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def send_welcome(message):
     btn_yt=types.InlineKeyboardButton("Мой YouTube-канал 🎬", url="https://youtube.com")
     btn_rules=types.InlineKeyboardButton("Правила чата", callback_data="show_rules")
     markup.add(btn_yt, btn_rules)
-    bot.reply_to(message, f"Здорово, {message.from_user.first_name}! 👋\nЯ твой личный модератор.Вибирай действие на кнопках снизу:", reply_markup=markup)
+    bot.send_message(message, f"Здорово, {message.from_user.first_name}! 👋\nЯ твой личный модератор.Вибирай действие на кнопках снизу:", reply_markup=markup)
 @bot.message_handler(commands=['dice'])
 def roll_dice(message):
     bot.send_dice(message.chat.id)
@@ -34,9 +34,9 @@ def unwarn_user(message):
         user_id = message.reply_to_message.from_user.id
         warnings[user_id] = 0
         save_warnings()
-        bot.reply_to(message, f"Амнистия! Предупреждения для @{message.reply_to_message.from_user.username} сброшены! 👍")
+        bot.send_message(message.chat.id, f"Амнистия! Предупреждения для @{message.reply_to_message.from_user.username} сброшены! 👍")
     else:
-        bot.reply_to(message, "Ответь командой /unwarn на сообщение того, кого хочешь помиловать.")
+        bot.send_message(message.chat.id, "Ответь командой /unwarn на сообщение того, кого хочешь помиловать.")
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data == "show_rules":
@@ -47,17 +47,21 @@ def filter_messages(message):
     text = message.text.lower()
     for word in bad_words:
         if word in text:
+            try:
+                bot.delete_message(message.chat.id, message.message_id)
+            except Exception as e:
+                print(f"Не удалось удалить сообщение:{є}")
             user_id = message.from_user.id
             warnings[user_id] = warnings.get(user_id, 0) + 1
             save_warnings()
             if warnings[user_id] >= 3:
-                bot.reply_to(message, "Ты нарушил правила 3 раза! Улетаешь в бан! 🚫")
+                bot.send_message(message.chat.id, f"Користувач {message.from_user.first_name} порушив правила 3 раза і відлітає в бан!")
                 try:
                     bot.ban_chat_member(message.chat.id, user_id)
                 except:
-                    bot.reply_to(message, "Я хотел тебя забанить, но у меня нет прав админа! 😅")
+                    bot.send_message(message.chat.id, "Я хотел тебя забанить, но у меня нет прав админа! 😅")
             else:
-                bot.reply_to(message, f"Ей! Нельзя такие слова писать! (Предупреждение {warnings[user_id]}/3)")
+                bot.send_message(message.chat.id, f"Попередження для {message.from_user.first_name}! У вас вже {warnings[user_id]}/3 варнів. Поводьтеся ввічливо!")
             break
 bot.polling(none_stop=True)
 
